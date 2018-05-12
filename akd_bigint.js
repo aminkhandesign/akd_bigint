802a0bc325794ed6f69d7afb6963e8a3849fe2a2
codeclub <codeclub@LAUSD-MBP-2017-4.local>
2018-05-11 22:24:16 -0700

    MAJOR UPDATE

--- akd_bigint.js
+++ akd_bigint.js
@@ -1,8 +1,6 @@
 
 
 
-
-
 function mult(firstnum1,secondnum1){ 
   let num1 = remove_point(firstnum1);
   let num2 = remove_point(secondnum1); 
@@ -169,9 +167,109 @@ function remove_point(a){
 }
 
 function trimnum(num){
-//let finalnum = num.replace(/\.$/,"").replace(/^0+/,"").replace(/(0+)$/,"");
-let finalnum = num.replace(/\.$|^0+|(0+)$/,"");
+let finalnum = num.replace(/\.$/,"").replace(/^0+/,"").replace(/(\.0+)$/,"").replace(/(\.\d+[1-9])(0+$)/, "$1");
+
+console.log(finalnum);
 return finalnum
 }
+
+
+function divide(num1,num2){
+
+let finalarr=[];
+let carry=0;
+let nums = [num1,num2];
+let test=nums[0];
+let l = 0;
+function mins(a,b){
+ return minus(a,b)
+ }
+while(test[0] !=="-"){
+ l++;
+  test =  minus(test,nums[1])
+
+}
+finalarr.push((l-1).toString()) 
+// if(test[0]==="-"){
+//   finalarr.push(".");
+//   finalarr
+//   }
+ 
+
+
+l=0;
+console.log(finalarr)
+return l-1
+}
+
+
+//divide("800","15")
+function quot(num1,num2){
+let count = 0;
+let init= num1;
+let last;
+let ans = ""
+while(ans[0]!="-"){
+ count++;
+ ans=minus(init,num2);
+ console.log(ans)
+ init=ans
+ 
+}
+let mod = Number(num2)+Number(init);
+return [String(count-1),String(mod)]
+}
+
+function correctLength(a,b){
+let diff = Math.abs(a.length-b.length);
+let num = a;
+let denom = b;
+let mag = [0,""];
+if(a.length<b.length)
+ {
+    let loop;
+   mag[1]="+";
+   if(a>=b){loop=diff}
+   else {loop=diff+1;mag[0]++}
+   for(let i=1;i<=loop;i++){
+     mag[0]++
+     num=num+"0"
+   }
+ }
+else if(a.length>b.length)
+ {
+    let loop;
+   mag[1]="-";
+   if(a>=b){loop=diff}
+   else {loop=diff-1}
+   for(let i=1;i<=loop;i++){
+     mag[0]++
+     denom=denom+"0"
+   }
+ }
+console.log(num,denom,mag)
+return [num,denom,mag]
+}
+
+function div(...args){
+
+num1 = trimnum(args[0]);
+num2 = trimnum(args[1]);
+let dee = num1;
+let dor = num2;
+let corrected = correctLength(dee,dor);
+dee = corrected[0];
+dor = corrected[1];
+let mag = corrected[2][0];
+let dir = corrected[2][1];
+
+res = quot(dee,dor)
+if(dir==="-"){for(i=0;i<mag;i++){res[0]=res[0]+"0"}}
+else if(dir==="+"){for(i=0;i<mag-1;i++){res[0]="0"+res[0]}};
+return res
+
+}
+
+
 
 
