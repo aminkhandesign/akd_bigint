97094a6779c8c39bdaa804b588a3e6aaad316b4e
Amin Khan <makart@live.co.uk>
2018-05-21 23:48:57 -0700

    SUITE OF MULT() TESTS ADDED

--- spec/akd_bigint.spec.js
+++ spec/akd_bigint.spec.js
@@ -2,8 +2,24 @@ var mult = require("../akd_bigint.js");
 
 describe("Multiplying two numbers",()=>{
 
-    it("two positive integers..", ()=>{
-        expect(mult("10","10")).toBe("100");
+    it("two single digit integers..", ()=>{
+        expect(mult("2","2")).toBe("4");
+    });
+
+    it("two large integers..",()=>{
+        expect(mult("2345678987654321","98765432123456789")).toBe("231671998838591676072245112635269");
+    });
+
+    it("one large, one small integer",()=>{
+        expect(mult("2345678987654321","5")).toBe("11728394938271605");
+    });
+    
+    it("two small floats..", ()=>{
+        expect(mult("3.03","3.03")).toBe("9.1809");
+    });
+
+    it("two large floats...", ()=>{
+        expect(mult("898967674545.121234345656","212143436565.989876765454")).toBe("190710091839738359022568.601697542009893475767824");
     });
 });
 
